a06fd4d9cd2ce49adbc191677f577596ec0f29b1
Till Willing <tillwilling@gmail.com>
2021-05-11 17:09:53 +0200

    created array and properties

--- src/Card.jsx
+++ src/Card.jsx
@@ -3,7 +3,7 @@ import './Card.css'
 
 export default ({ question, answer, active, id }) => {
   return (
-    <section className="Card">
+    <section className="Card" id={id}>
       <h2>{question}</h2>
       <p>{answer}</p>
       <div className={active ? 'bookmark active' : 'bookmark'}></div>
