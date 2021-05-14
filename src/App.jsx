dd593d7be8bde58fcaf178784ae46e712d5fe6b8
Till Willing <tillwilling@gmail.com>
2021-05-14 10:40:18 +0200

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,26 @@
 import React, { useState } from 'react'
-import { v4 as uuidv4 } from 'uuid'
 import './App.css'
 
 export default () => {
-  const [todos, setTodos] = useState([])
+  const [size, setSize] = useState(100)
+
+  const style = {
+    width: size + 'px',
+    height: size + 'px',
+  }
+
   return (
     <div className="App">
-      <form onSubmit={handleSubmit}>
-        <label>
-          Add todo:
-          <input name="todo" type="text" />
-        </label>
-        <button>Add</button>
-      </form>
-      <ul>
-        {todos.map(({ text, isDone, id }) => (
-          <li onClick={() => toggleIsDone(id)} key={id}>
-            {text} {isDone && '✅'} 
-          </li>
-        ))}
-      </ul>
+      <label>
+        Size:
+        <input value={size} onChange={handleChange} type="range" max="200" />
+      </label>
+      <div style={style} className="Box" />
     </div>
   )
 
-  function toggleIsDone(id) {
-    const index = todos.findIndex(todo => todo.id === id)
-    const todo = todos[index]
-    setTodos([
-      ...todos.slice(0, index),
-      { ...todo, isDone: !todo.isDone },
-      ...todos.slice(index + 1),
-    ])
-  }
-
-  function handleSubmit(event) {
-    event.preventDefault()
-    const form = event.target
-    const input = form.elements.todo
-    const newTodo = { text: input.value, isDone: false, id: uuidv4() }
-    setTodos([newTodo, ...todos])
-    form.reset()
-    input.focus()
+  function handleChange(event) {
+    const input = event.target
+    setSize(input.value)
   }
 }
